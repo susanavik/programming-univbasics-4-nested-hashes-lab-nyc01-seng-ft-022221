ce88bf72681ccb26ad0d52ff77ac02ab7503e5f2
Susana vik <susana.m.vik@gmail.com>
2021-02-10 05:12:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-		programmer_hash[:alan_kay][:known_for] = GUI
+		programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
